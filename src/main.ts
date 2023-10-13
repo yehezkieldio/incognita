8b25a2792dcb87ad283bd1b6a4be8e26039124d7
elizielx <yehezkieldio35@gmail.com>
2023-10-13 23:07:50 +0800

    feat: stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app.module";
+import { Config } from "./config";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    await app.listen(Config.PORT);
 }
 bootstrap();
